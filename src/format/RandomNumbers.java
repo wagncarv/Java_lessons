35fd0c113f83451a009fef9a2610f21f94ea4690
Wagner Patrick de Carvalho <wagncarv@gmail.com>
2021-07-04 19:25:31 -0300

    random

--- src/format/RandomNumbers.java
+++ src/format/RandomNumbers.java
@@ -13,10 +13,18 @@ public class RandomNumbers {
 		for (int i = 0; i < 10; i++) {
 //			double d = Math.random();
 //			int d = r.nextInt();
-			int d = r.nextInt(100);
-			System.out.println((i + 1) + " => " + d);
+//			int d = r.nextInt(100);
+			int d = gerarInt(50, 70);
+			System.out.println(d);
+//			System.out.println((i + 1) + " => " + d);
 		}
 
+	}
+	
+	static int gerarInt(int inicio, int fim) {
+		int intervalo = fim - inicio;
+		int n = (int) (Math.random() * intervalo) + inicio;
+		return n;
 	}
 
 }
